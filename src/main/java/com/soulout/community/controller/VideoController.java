b942374a7dd8031fa43dcb13f5ee6558d0e812e0
WincH1994 <a13202461380@gmail.com>
2020-03-13 11:21:52 +0800

    add video page

--- src/main/java/com/soulout/community/controller/VideoController.java
+++ src/main/java/com/soulout/community/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.soulout.community.controller;
 import com.soulout.community.cache.HotTagCache;
 import com.soulout.community.dto.PaginationDTO;
 import com.soulout.community.service.QuestionService;
+import org.bouncycastle.math.raw.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,8 +21,12 @@ public class VideoController {
         return "videos";
     }
 
-    @GetMapping("/video/{id}")
-    public String video(@PathVariable(name = "id") String id){
+    @GetMapping("/video")
+    public String video(@RequestParam(name = "aid") String aid,
+                        @RequestParam(name = "page",defaultValue = "1") String page,
+                        Model model){
+        model.addAttribute("aid",aid);
+        model.addAttribute("page",page);
         return "video";
     }
 }
